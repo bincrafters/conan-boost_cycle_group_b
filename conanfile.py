8623a52191b070bcab8bd229395b6063ae8f107d
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:11:25 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,38 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostCycleGroupBConan(base.BoostBaseConan):
-    name = "boost_cycle_group_b" # Level 12
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_cycle_group_b"
-    lib_short_names = [
-        "lexical_cast",
-        "math"
-    ]
-    header_only_libs = ["lexical_cast"]
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
-    b2_requires = [
-        "boost_array",
-        "boost_assert",
-        "boost_atomic",
-        "boost_concept_check",
-        "boost_config",
-        "boost_container",
-        "boost_core",
-        "boost_detail",
-        "boost_fusion",
-        "boost_integer",
-        "boost_lambda",
-        "boost_mpl",
-        "boost_numeric_conversion",
-        "boost_predef",
-        "boost_range",
-        "boost_smart_ptr",
-        "boost_static_assert",
-        "boost_throw_exception",
-        "boost_tuple",
-        "boost_type_traits"
-    ]
+    name = "boost_cycle_group_b"
+    version = "1.70.0"
